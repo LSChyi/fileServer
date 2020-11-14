0b161e00953a75c79ec345bd9f84beea0c798e7a
LSChyi <alan81920@gmail.com>
2020-11-14 23:44:02 +0800

    add switch for upload function

--- main.go
+++ main.go
@@ -14,27 +14,37 @@ import (
 type fileServerWrapper struct {
 	listHandler   http.Handler
 	uploadHandler http.Handler
+	enableUpload  bool
 }
 
-func NewFileServer(path string) (*fileServerWrapper, error) {
+func NewFileServer(path string, uploadable bool) (*fileServerWrapper, error) {
 	fileServer := handler.NewFileServer(path)
 	listHandler := handlermiddleware.NewListHandler(fileServer)
-	uploadHandler, err := handlermiddleware.NewUploadHandler(fileServer, path)
-	if err != nil {
-		return nil, err
+	var uploadHandler http.Handler
+	var err error
+	if uploadable {
+		uploadHandler, err = handlermiddleware.NewUploadHandler(fileServer, path)
+		if err != nil {
+			return nil, err
+		}
 	}
 	return &fileServerWrapper{
 		listHandler:   listHandler,
 		uploadHandler: uploadHandler,
+		enableUpload:  uploadable,
 	}, nil
 }
 
 func (f *fileServerWrapper) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	var writer http.ResponseWriter
 	writer = responsemiddleware.NewLogMiddleware(w, req)
-	writer = responsemiddleware.NewUploadMiddleware(writer)
-	if req.Method == http.MethodPost {
-		f.uploadHandler.ServeHTTP(writer, req)
+	if f.enableUpload {
+		writer = responsemiddleware.NewUploadMiddleware(writer)
+		if req.Method == http.MethodPost {
+			f.uploadHandler.ServeHTTP(writer, req)
+		} else {
+			f.listHandler.ServeHTTP(writer, req)
+		}
 	} else {
 		f.listHandler.ServeHTTP(writer, req)
 	}
@@ -43,14 +53,15 @@ func (f *fileServerWrapper) ServeHTTP(w http.ResponseWriter, req *http.Request) 
 func main() {
 	port := flag.String("p", "8000", "port to serve")
 	directory := flag.String("d", ".", "directory to server")
+	uploadable := flag.Bool("u", true, "enable upload function")
 	flag.Parse()
 
-	server, err := NewFileServer(*directory)
+	server, err := NewFileServer(*directory, *uploadable)
 	if err != nil {
 		log.WithError(err).Fatal("can not create file server")
 	}
 	http.Handle("/", server)
-	log.WithField("port", *port).WithField("directory", *directory).Info("Serving file server")
+	log.WithField("port", *port).WithField("directory", *directory).WithField("enable upload", *uploadable).Info("Serving file server")
 	if err := http.ListenAndServe(":"+*port, nil); err != nil {
 		log.WithError(err).Fatal("encounter error")
 	}
